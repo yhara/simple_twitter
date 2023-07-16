fe219f0a3128aee2737ee74df0a4a62cdd8c6a5d
sue445 <sue445@sue445.net>
2023-07-16 15:14:10 +0900

    Add test for post with API v2

--- spec/simple_twitter/client_spec.rb
+++ spec/simple_twitter/client_spec.rb
@@ -44,4 +44,27 @@ RSpec.describe SimpleTwitter::Client do
     its([:data, :id]) { should eq "1445880548472328192" }
     its([:data, :text]) { should eq "Are you excited for the weekend?" }
   end
+
+  describe "error with API v2" do
+    subject { client.get("https://api.twitter.com/2/users/me") }
+
+    let(:client) { SimpleTwitter::Client.new(bearer_token: "invalid_bearer_token") }
+
+    before do
+      stub_request(:get, "https://api.twitter.com/2/users/me").
+        with(
+          headers: {
+            'Authorization'=>'Bearer invalid_bearer_token',
+          }).
+        to_return(status: 200, body: fixture("error_403.json"))
+    end
+
+    it "get response" do
+      tweets = subject[:data]
+
+      expect(tweets.count).to eq 3
+      expect(tweets[0][:id]).to eq "1460323737035677698"
+      expect(tweets[0][:text]).to eq "Introducing a new era for the Twitter Developer Platform! nn📣The Twitter API v2 is now the primary API and full of new featuresn⏱Immediate access for most use cases, or apply to get more access for freen📖Removed certain restrictions in the Policynhttps://t.co/Hrm15bkBWJ https://t.co/YFfCDErHsg"
+    end
+  end
 end
